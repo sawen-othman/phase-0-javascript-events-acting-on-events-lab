d0e20da547daa7264f72d333857d8068016cf789
sawen <sawenothman2001@gmail.com>
2022-07-31 22:29:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function moveDodgerLeft(){
+    const dodger = document.getElementById("dodger");
+    dodger.style.bottom = "0px";
+    dodger.style.left = "0px";
+}
+
+function moveDodgerRight(){
+    const dodger = document.getElementById("dodger");
+    dodger.style.bottom = "0px";
+    dodger.style.left = "100px";
+}
